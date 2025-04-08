d5a1503d42d90679a440ccef28c365f9ca605b6a
Trung Kiên Trần Nguyễn <cristntk0162@gmail.com>
2025-04-08 12:22:17 +0700

    update deploy script

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "🛑 Stopping all services..."
+sudo systemctl stop kafka_server
+sudo systemctl stop socket_server
+sudo systemctl stop http_server
+
+echo "📥 Pulling latest code..."
+cd /home/doanvanquoc/CRM-2025
+git pull origin main
+
 echo "🔧 Building HTTP server..."
-cd /home/doanvanquoc/CRM-2025/http_server
+cd http_server
 /usr/local/go/bin/go build -o http_server main.go
 chmod +x http_server
 
@@ -17,15 +26,6 @@ cd ..
 cd kafka_server
 /usr/local/go/bin/go build -o kafka_server consumer.go
 chmod +x kafka_server
-
-echo "🛑 Stopping all services..."
-sudo systemctl stop kafka_server
-sudo systemctl stop socket_server
-sudo systemctl stop http_server
-
-echo "📥 Pulling latest code..."
-cd ..
-git pull origin main
 
 echo "✅ Starting services..."
 sudo systemctl start kafka_server
