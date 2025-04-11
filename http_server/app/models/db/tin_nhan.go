d6adc010cbc5e3799f9901fccfd92e29c70d9511
Trung Kiên Trần Nguyễn <cristntk0162@gmail.com>
2025-04-11 16:45:50 +0700

    update struct tin nhan

--- http_server/app/models/db/tin_nhan.go
+++ http_server/app/models/db/tin_nhan.go
@@ -10,6 +10,6 @@ type Tin_nhan struct {
 	Content			int			`json:"content"`
 	Is_read			bool		`json:"is_read"`
 
-	Sender			Nhan_vien	`json:"sender" gorm:"foreignKey:Nhan_vien_id"`
-	Receiver		Nhan_vien	`json:"receiver" gorm:"foreignKey:Nhan_vien_id"`
+	Sender			Nhan_vien	`json:"sender" gorm:"foreignKey:Sender_id"`
+	Receiver		Nhan_vien	`json:"receiver" gorm:"foreignKey:Receiver_id"`
 }
