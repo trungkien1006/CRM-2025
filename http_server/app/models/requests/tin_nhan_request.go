92713e03f6bad3406ad26ef48e26ca60e539fdc6
Trung Kiên Trần Nguyễn <cristntk0162@gmail.com>
2025-04-14 14:44:07 +0700

    update message struct

--- http_server/app/models/requests/tin_nhan_request.go
+++ http_server/app/models/requests/tin_nhan_request.go
@@ -3,7 +3,7 @@ package requests
 type Tin_nhan_create struct {
 	Sender_id		int					`json:"sender_id" binding:"required"`
 	Receiver_id		int					`json:"receiver_id" binding:"required"`
-	Content			int					`json:"content" binding:"required"`
+	Content			string				`json:"content" binding:"required"`
 }
 
 type Tin_nhan_create_batch struct {
--- http_server/app/models/responses/tin_nhan_response.go
+++ http_server/app/models/responses/tin_nhan_response.go
@@ -4,7 +4,7 @@ type Tin_nhan_filter struct {
 	ID          	int    		`json:"ID"`
 	Sender_id   	int    		`json:"sender_id"`
 	Receiver_id 	int    		`json:"receiver_id"`
-	Content     	int    		`json:"content"`
+	Content     	string   	`json:"content"`
 	Is_read     	bool   		`json:"is_read"`
 	Created_at  	string 		`json:"created_at"`
 }
